a53a74b8c7161a20bf0d30061b0bbdaf1fc8b1e6
Elias Carvalho <eliascarvdev@gmail.com>
2023-11-13 17:16:47 -0300

    Add tests

--- src/extra/vtkwrite.jl
+++ src/extra/vtkwrite.jl
@@ -2,14 +2,6 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-vtktype(::Type{<:Segment}) = VTKCellTypes.VTK_LINE
-vtktype(::Type{<:Triangle}) = VTKCellTypes.VTK_TRIANGLE
-vtktype(::Type{<:Quadrangle}) = VTKCellTypes.VTK_QUAD
-vtktype(::Type{<:Ngon}) = VTKCellTypes.VTK_POLYGON
-vtktype(::Type{<:Tetrahedron}) = VTKCellTypes.VTK_TETRA
-vtktype(::Type{<:Hexahedron}) = VTKCellTypes.VTK_HEXAHEDRON
-vtktype(::Type{<:Pyramid}) = VTKCellTypes.VTK_PYRAMID
-
 function vtkwrite(fname, geotable)
   dom = domain(geotable)
   etable = values(geotable)
@@ -34,7 +26,7 @@ function vtuwrite(fname, mesh::SimpleMesh, etable, vtable)
   verts = vertices(mesh)
   connec = elements(topology(mesh))
   points = stack(coordinates, verts)
-  cells = map(c -> (vtktype(pltype(c)), indices(c)), connec)
+  cells = map(c -> VTKBase.MeshCell(_vtktype(pltype(c)), indices(c)), connec)
 
   WriteVTK.vtk_grid(fname, points, cells) do vtk
     _writetables(vtk, etable, vtable)
@@ -45,7 +37,7 @@ function vtpwrite(fname, mesh::SimpleMesh, etable, vtable)
   verts = vertices(mesh)
   connec = elements(topology(mesh))
   points = stack(coordinates, verts)
-  cells = map(c -> (WriteVTK.PolyData.Polys(), indices(c)), connec)
+  cells = map(c -> VTKBase.MeshCell(PolyData.Polys(), indices(c)), connec)
 
   WriteVTK.vtk_grid(fname, points, cells) do vtk
     _writetables(vtk, etable, vtable)
@@ -69,7 +61,7 @@ function vtiwrite(fname, grid::CartesianGrid, etable, vtable)
   spac = spacing(grid)
   dims = size(grid)
   xyz = map(orig, spac, dims) do o, s, d 
-    range(start=o, step=s, length=d)
+    range(start=o, step=s, length=(d + 1))
   end
   WriteVTK.vtk_grid(fname, xyz...) do vtk
     _writetables(vtk, etable, vtable)
@@ -79,6 +71,14 @@ end
 #-------
 # UTILS
 #-------
+
+_vtktype(::Type{<:Segment}) = VTKCellTypes.VTK_LINE
+_vtktype(::Type{<:Triangle}) = VTKCellTypes.VTK_TRIANGLE
+_vtktype(::Type{<:Quadrangle}) = VTKCellTypes.VTK_QUAD
+_vtktype(::Type{<:Ngon}) = VTKCellTypes.VTK_POLYGON
+_vtktype(::Type{<:Tetrahedron}) = VTKCellTypes.VTK_TETRA
+_vtktype(::Type{<:Hexahedron}) = VTKCellTypes.VTK_HEXAHEDRON
+_vtktype(::Type{<:Pyramid}) = VTKCellTypes.VTK_PYRAMID
 
 function _writetables(vtk, etable, vtable)
   _writetable(vtk, VTKBase.VTKCellData(), etable)
