0887fdb04f2cda9975e56fe42ea35ea6fdf0bfa5
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2023-12-12 17:33:29 -0300

    Add support for STL Binary format (#43)
    * Add support for STL Binary format
    
    * Use NTuple to store points
    
    * Update code style

--- src/extra/stl.jl
+++ src/extra/stl.jl
@@ -2,19 +2,33 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-function stlraed(fname)
-  # TODO STL Binary
-  stlasciiread(fname)
-end
+# ---------
+# STL READ
+# ---------
 
-function stlwrite(fname, geotable)
-  # TODO STL Binary
-  stlasciiwrite(fname, geotable)
+function stlraed(fname)
+  normals, vertices = if _isstlascii(fname)
+    stlasciiread(fname)
+  else
+    stlbinread(fname)
+  end
+
+  upoints = unique(Iterators.flatten(vertices))
+  ptindex = Dict(zip(upoints, eachindex(upoints)))
+  connec = map(vertices) do points
+    inds = ntuple(i -> ptindex[points[i]], 3)
+    connect(inds, Triangle)
+  end
+
+  mesh = SimpleMesh(upoints, connec)
+  table = (; NORMAL=normals)
+
+  georef(table, mesh)
 end
 
 function stlasciiread(fname)
   normals = Vec3[]
-  vertices = Vector{Point3}[]
+  vertices = NTuple{3,Point3}[]
 
   open(fname) do io
     readline(io) # skip header
@@ -26,7 +40,7 @@ function stlasciiread(fname)
         push!(normals, normal)
 
         readline(io) # skip outer loop
-        points = map(1:3) do _
+        points = ntuple(3) do _
           coords = _splitline(io)[2:end]
           Point(_parsecoords(coords))
         end
@@ -38,25 +52,49 @@ function stlasciiread(fname)
     end
   end
 
-  upoints = unique(Iterators.flatten(vertices))
-  connec = map(vertices) do points
-    inds = indexin(points, upoints)
-    connect(Tuple(inds), Triangle)
-  end
-
-  mesh = SimpleMesh(upoints, connec)
-  table = (; NORMAL=normals)
-
-  georef(table, mesh)
+  normals, vertices
 end
 
-function stlasciiwrite(fname, geotable)
+function stlbinread(fname)
+  normals = Vec3f[]
+  vertices = NTuple{3,Point3f}[]
+
+  open(fname) do io
+    skip(io, 80) # skip header
+    ntriangles = read(io, UInt32)
+    for _ in 1:ntriangles
+      normal = Vec(ntuple(i -> read(io, Float32), 3))
+      push!(normals, normal)
+      points = ntuple(3) do _
+        Point(ntuple(i -> read(io, Float32), 3))
+      end
+      push!(vertices, points)
+      skip(io, 2) # skip attribute byte count
+    end
+  end
+
+  normals, vertices
+end
+
+# ----------
+# STL WRITE
+# ----------
+
+function stlwrite(fname, geotable; ascii=false)
   mesh = domain(geotable)
 
   if !(embeddim(mesh) == 3 && eltype(mesh) <: Triangle)
     throw(ArgumentError("STL format only supports 3D triangle meshes"))
   end
 
+  if ascii
+    stlasciiwrite(fname, mesh)
+  else
+    stlbinwrite(fname, mesh)
+  end
+end
+
+function stlasciiwrite(fname, mesh)
   # file name for header
   name = first(splitext(basename(fname)))
 
@@ -70,7 +108,6 @@ function stlasciiwrite(fname, geotable)
     for triangle in elements(mesh)
       n = normal(triangle)
       write(io, "facet normal $(frmtcoords(n))\n")
-
       write(io, "    outer loop\n")
 
       for point in vertices(triangle)
@@ -84,6 +121,38 @@ function stlasciiwrite(fname, geotable)
 
     write(io, "endsolid $name\n")
   end
+end
+
+function stlbinwrite(fname, mesh)
+  open(fname, write=true) do io
+    foreach(i -> write(io, 0x00), 1:80) # empty header
+
+    write(io, UInt32(nelements(mesh))) # number of triangles
+
+    for triangle in elements(mesh)
+      n = normal(triangle)
+      foreach(c -> write(io, Float32(c)), n)
+      for point in vertices(triangle)
+        foreach(c -> write(io, Float32(c)), coordinates(point))
+      end
+      write(io, 0x0000) # empty attribute byte count
+    end
+  end
+end
+
+# -----------------
+# HELPER FUNCTIONS
+# -----------------
+
+function _isstlascii(fname)
+  result = false
+  open(fname) do io
+    line = readline(io)
+    if startswith(line, "solid")
+      result = true
+    end
+  end
+  result
 end
 
 _splitline(io) = split(lowercase(readline(io)))
