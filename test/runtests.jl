aedc6d3d710a47a50abc7bc5b4e5da81c42bff6e
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2023-12-22 15:28:16 -0300

    Add support for loading and saving OBJ files (#57)

--- test/runtests.jl
+++ test/runtests.jl
@@ -152,6 +152,15 @@ end
       @test length(gtb.geometry) == 4
     end
 
+    @testset "OBJ" begin
+      gtb = GeoIO.load(joinpath(datadir, "tetrahedron.obj"))
+      @test gtb.geometry isa SimpleMesh
+      @test embeddim(gtb.geometry) == 3
+      @test coordtype(gtb.geometry) <: Float64
+      @test eltype(gtb.geometry) <: Triangle
+      @test length(gtb.geometry) == 4
+    end
+
     @testset "PLY" begin
       table = GeoIO.load(joinpath(datadir, "beethoven.ply"))
       @test table.geometry isa SimpleMesh
@@ -499,6 +508,21 @@ end
       # error: STL format only supports 3D triangle meshes
       gtb = GeoTable(CartesianGrid(2, 2, 2))
       file = joinpath(savedir, "error.stl")
+      @test_throws ArgumentError GeoIO.save(file, gtb)
+    end
+
+    @testset "OBJ" begin
+      file1 = joinpath(datadir, "tetrahedron.obj")
+      file2 = joinpath(savedir, "tetrahedron.obj")
+      gtb1 = GeoIO.load(file1)
+      GeoIO.save(file2, gtb1)
+      gtb2 = GeoIO.load(file2)
+      @test gtb1 == gtb2
+      @test values(gtb1, 0) == values(gtb2, 0)
+
+      # error: OBJ format only supports 3D Ngon meshes
+      gtb = GeoTable(CartesianGrid(2, 2, 2))
+      file = joinpath(savedir, "error.obj")
       @test_throws ArgumentError GeoIO.save(file, gtb)
     end
 
