c52a742788832ccb2f678b38b767213a8909ed9b
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2024-01-02 15:15:31 -0300

    Add support for loading and saving OFF files (#61)

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Meshes
 using GeoTables
 using Test, Random
 using ReferenceTests
+using Colors
 using Dates
 import ReadVTK
 import GeoInterface as GI
@@ -154,6 +155,16 @@ end
 
     @testset "OBJ" begin
       gtb = GeoIO.load(joinpath(datadir, "tetrahedron.obj"))
+      @test gtb.geometry isa SimpleMesh
+      @test embeddim(gtb.geometry) == 3
+      @test coordtype(gtb.geometry) <: Float64
+      @test eltype(gtb.geometry) <: Triangle
+      @test length(gtb.geometry) == 4
+    end
+
+    @testset "OFF" begin
+      gtb = GeoIO.load(joinpath(datadir, "tetrahedron.off"))
+      @test eltype(gtb.COLOR) <: RGBA{Float64}
       @test gtb.geometry isa SimpleMesh
       @test embeddim(gtb.geometry) == 3
       @test coordtype(gtb.geometry) <: Float64
@@ -524,6 +535,26 @@ end
       gtb = GeoTable(CartesianGrid(2, 2, 2))
       file = joinpath(savedir, "error.obj")
       @test_throws ArgumentError GeoIO.save(file, gtb)
+    end
+
+    @testset "OFF" begin
+      file1 = joinpath(datadir, "tetrahedron.off")
+      file2 = joinpath(savedir, "tetrahedron.off")
+      gtb1 = GeoIO.load(file1)
+      GeoIO.save(file2, gtb1, color=:COLOR)
+      gtb2 = GeoIO.load(file2)
+      @test gtb1 == gtb2
+      @test values(gtb1, 0) == values(gtb2, 0)
+
+      # error: OFF format only supports 3D Ngon meshes
+      gtb = GeoTable(CartesianGrid(2, 2, 2))
+      file = joinpath(savedir, "error.off")
+      @test_throws ArgumentError GeoIO.save(file, gtb)
+      # error: color column must be a iterable of colors
+      mesh = domain(gtb1)
+      gtb = georef((; COLOR=rand(4)), mesh)
+      file = joinpath(savedir, "error.off")
+      @test_throws ArgumentError GeoIO.save(file, gtb, color="COLOR")
     end
 
     @testset "PLY" begin
