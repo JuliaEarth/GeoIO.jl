57076cf15b16b960163619071dda2c7e08da72d8
Elias Carvalho <eliascarvdev@gmail.com>
2024-06-07 15:34:14 -0300

    Split runtests.jl in multiple files

---
+++ test/convert.jl
@@ -0,0 +1,76 @@
+@testset "convert" begin
+  points = Point.([(0, 0), (2.2, 2.2), (0.5, 2)])
+  outer = Point.([(0, 0), (2.2, 2.2), (0.5, 2), (0, 0)])
+
+  # GI functions
+  @test GI.ngeom(Segment(points[1], points[2])) == 2
+  @test GI.ngeom(Rope(points)) == 3
+  @test GI.ngeom(Ring(points)) == 4
+  @test GI.ngeom(PolyArea(points)) == 1
+  @test GI.ngeom(Multi(points)) == 3
+  @test GI.ngeom(Multi([Rope(points), Rope(points)])) == 2
+  @test GI.ngeom(Multi([PolyArea(points), PolyArea(points)])) == 2
+
+  # Shapefile.jl
+  ps = [SHP.Point(0, 0), SHP.Point(2.2, 2.2), SHP.Point(0.5, 2)]
+  exterior = [SHP.Point(0, 0), SHP.Point(2.2, 2.2), SHP.Point(0.5, 2), SHP.Point(0, 0)]
+  box = SHP.Rect(0.0, 0.0, 2.2, 2.2)
+  point = SHP.Point(1.0, 1.0)
+  chain = SHP.LineString{SHP.Point}(view(ps, 1:3))
+  poly = SHP.SubPolygon([SHP.LinearRing{SHP.Point}(view(exterior, 1:4))])
+  multipoint = SHP.MultiPoint(box, ps)
+  multichain = SHP.Polyline(box, [0, 3], repeat(ps, 2))
+  multipoly = SHP.Polygon(box, [0, 4], repeat(exterior, 2))
+  @test GeoIO.geom2meshes(point) == Point(1.0, 1.0)
+  @test GeoIO.geom2meshes(chain) == Rope(points)
+  @test GeoIO.geom2meshes(poly) == PolyArea(points)
+  @test GeoIO.geom2meshes(multipoint) == Multi(points)
+  @test GeoIO.geom2meshes(multichain) == Multi([Rope(points), Rope(points)])
+  @test GeoIO.geom2meshes(multipoly) == Multi([PolyArea(points), PolyArea(points)])
+  # degenerate chain with 2 equal points
+  ps = [SHP.Point(2.2, 2.2), SHP.Point(2.2, 2.2)]
+  chain = SHP.LineString{SHP.Point}(view(ps, 1:2))
+  @test GeoIO.geom2meshes(chain) == Ring((2.2, 2.2))
+
+  # ArchGDAL.jl
+  ps = [(0, 0), (2.2, 2.2), (0.5, 2)]
+  outer = [(0, 0), (2.2, 2.2), (0.5, 2), (0, 0)]
+  point = AG.createpoint(1.0, 1.0)
+  chain = AG.createlinestring(ps)
+  poly = AG.createpolygon(outer)
+  multipoint = AG.createmultipoint(ps)
+  multichain = AG.createmultilinestring([ps, ps])
+  multipoly = AG.createmultipolygon([[outer], [outer]])
+  polyarea = PolyArea(outer[begin:(end - 1)])
+  @test GeoIO.geom2meshes(point) == Point(1.0, 1.0)
+  @test GeoIO.geom2meshes(chain) == Rope(points)
+  @test GeoIO.geom2meshes(poly) == polyarea
+  @test GeoIO.geom2meshes(multipoint) == Multi(points)
+  @test GeoIO.geom2meshes(multichain) == Multi([Rope(points), Rope(points)])
+  @test GeoIO.geom2meshes(multipoly) == Multi([polyarea, polyarea])
+  # degenerate chain with 2 equal points
+  chain = AG.createlinestring([(2.2, 2.2), (2.2, 2.2)])
+  @test GeoIO.geom2meshes(chain) == Ring((2.2, 2.2))
+
+  # GeoJSON.jl
+  points = Point.([(0.0f0, 0.0f0), (2.2f0, 2.2f0), (0.5f0, 2.0f0)])
+  outer = Point.([(0.0f0, 0.0f0), (2.2f0, 2.2f0), (0.5f0, 2.0f0)])
+  point = GJS.read("""{"type":"Point","coordinates":[1,1]}""")
+  chain = GJS.read("""{"type":"LineString","coordinates":[[0,0],[2.2,2.2],[0.5,2]]}""")
+  poly = GJS.read("""{"type":"Polygon","coordinates":[[[0,0],[2.2,2.2],[0.5,2],[0,0]]]}""")
+  multipoint = GJS.read("""{"type":"MultiPoint","coordinates":[[0,0],[2.2,2.2],[0.5,2]]}""")
+  multichain =
+    GJS.read("""{"type":"MultiLineString","coordinates":[[[0,0],[2.2,2.2],[0.5,2]],[[0,0],[2.2,2.2],[0.5,2]]]}""")
+  multipoly = GJS.read(
+    """{"type":"MultiPolygon","coordinates":[[[[0,0],[2.2,2.2],[0.5,2],[0,0]]],[[[0,0],[2.2,2.2],[0.5,2],[0,0]]]]}"""
+  )
+  @test GeoIO.geom2meshes(point) == Point(1.0f0, 1.0f0)
+  @test GeoIO.geom2meshes(chain) == Rope(points)
+  @test GeoIO.geom2meshes(poly) == PolyArea(outer)
+  @test GeoIO.geom2meshes(multipoint) == Multi(points)
+  @test GeoIO.geom2meshes(multichain) == Multi([Rope(points), Rope(points)])
+  @test GeoIO.geom2meshes(multipoly) == Multi([PolyArea(outer), PolyArea(outer)])
+  # degenerate chain with 2 equal points
+  chain = GJS.read("""{"type":"LineString","coordinates":[[2.2,2.2],[2.2,2.2]]}""")
+  @test GeoIO.geom2meshes(chain) == Ring(Point(2.2f0, 2.2f0))
+end
---
+++ test/noattrs.jl
@@ -0,0 +1,64 @@
+@testset "GeoTables without attributes" begin
+  pset = PointSet(rand(Point{2}, 10))
+  gtb1 = georef(nothing, pset)
+
+  # Shapefile
+  file = joinpath(savedir, "noattribs.shp")
+  GeoIO.save(file, gtb1)
+  gtb2 = GeoIO.load(file)
+  @test all(ismissing, gtb2[:, 1])
+  @test gtb2.geometry == gtb1.geometry
+
+  # GeoJSON
+  file = joinpath(savedir, "noattribs.geojson")
+  GeoIO.save(file, gtb1)
+  gtb2 = GeoIO.load(file, numbertype=Float64)
+  @test isnothing(values(gtb2))
+  @test gtb2 == gtb1
+
+  # GeoParquet
+  file = joinpath(savedir, "noattribs.parquet")
+  GeoIO.save(file, gtb1)
+  gtb2 = GeoIO.load(file)
+  @test isnothing(values(gtb2))
+  @test gtb2 == gtb1
+
+  # GeoPackage
+  file = joinpath(savedir, "noattribs.gpkg")
+  GeoIO.save(file, gtb1)
+  gtb2 = GeoIO.load(file)
+  @test isnothing(values(gtb2))
+  @test gtb2 == gtb1
+
+  # CSV
+  file = joinpath(savedir, "noattribs.csv")
+  GeoIO.save(file, gtb1)
+  gtb2 = GeoIO.load(file, coords=[:x, :y])
+  @test isnothing(values(gtb2))
+  @test gtb2 == gtb1
+
+  # GSLIB
+  file = joinpath(savedir, "noattribs.gslib")
+  GeoIO.save(file, gtb1)
+  gtb2 = GeoIO.load(file)
+  @test isnothing(values(gtb2))
+  @test gtb2 == gtb1
+
+  # VTK
+  file = joinpath(savedir, "noattribs.vts")
+  gtb1 = georef(nothing, CartesianGrid(10, 10))
+  GeoIO.save(file, gtb1)
+  gtb2 = GeoIO.load(file)
+  @test isnothing(values(gtb2))
+  @test gtb2 == gtb1
+
+  # MSH
+  gtb = GeoIO.load(joinpath(datadir, "tetrahedron1.msh"))
+  mesh = gtb.geometry
+  file = joinpath(savedir, "noattribs.msh")
+  gtb1 = georef(nothing, mesh)
+  GeoIO.save(file, gtb1)
+  gtb2 = GeoIO.load(file)
+  @test isnothing(values(gtb2))
+  @test gtb2 == gtb1
+end
