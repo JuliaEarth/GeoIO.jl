8b598dd917766ac70ce3cf1a3c770f1fdeb64a0f
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2024-08-08 10:14:35 -0300

    Speculative: Add internal `Reinterpret` transform for GeoTIFF use case (#107)
    * Add internal 'Reinterpret' transform for GeoTIFF use case
    
    * Apply suggestions
    
    * Update comments

--- src/extra/geotiff.jl
+++ src/extra/geotiff.jl
@@ -2,6 +2,17 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
+# this transform is used internally to reinterpret the CRS of points using raw coordinate values
+# it also flips the coordinates into a "xy" order as this is assumed by geotiff and other formats
+struct Reinterpret{CRS} <: CoordinateTransform end
+
+Reinterpret(CRS) = Reinterpret{CRS}()
+
+Meshes.applycoord(::Reinterpret{CRS}, p::Point) where {CRS} = Point(_reinterpret(CRS, CoordRefSystems.raw(coords(p))))
+
+_reinterpret(::Type{CRS}, raw) where {CRS} = CRS(raw...)
+_reinterpret(::Type{CRS}, (x, y)) where {CRS<:LatLon} = CRS(y, x)
+
 function geotiffread(fname; kwargs...)
   dataset = AG.read(fname; kwargs...)
   crs = AG.getproj(dataset)
@@ -11,7 +22,7 @@ function geotiffread(fname; kwargs...)
   # GDAL transform:
   # xnew = gt[1] + x * gt[2] + y * gt[3]
   # ynew = gt[4] + x * gt[5] + y * gt[6]
-  pipe = Affine(SA[gt[2] gt[3]; gt[5] gt[6]], SA[gt[1], gt[4]]) → Proj(CRS)
+  pipe = Affine(SA[gt[2] gt[3]; gt[5] gt[6]], SA[gt[1], gt[4]]) → Reinterpret(CRS)
   domain = CartesianGrid(dims) |> pipe
   pairs = map(1:AG.nraster(dataset)) do i
     name = Symbol(:BAND, i)
