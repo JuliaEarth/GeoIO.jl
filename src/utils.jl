15f63e2113ac19aa49ada5ab344652dfd9cfd71d
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-03-21 10:59:22 -0300

    Use CoordRefSystems.wkt2 instead of GDAL

--- src/utils.jl
+++ src/utils.jl
@@ -51,14 +51,6 @@ function uniquenames(names, newnames)
   end
 end
 
-function wktstring(code; format="WKT2", multiline=false)
-  spref = spatialref(code)
-  wktptr = Ref{Cstring}()
-  options = ["FORMAT=$format", "MULTILINE=$(multiline ? "YES" : "NO")"]
-  GDAL.osrexporttowktex(spref, wktptr, options)
-  unsafe_string(wktptr[])
-end
-
 function projjsonstring(code; multiline=false)
   spref = spatialref(code)
   wktptr = Ref{Cstring}()
