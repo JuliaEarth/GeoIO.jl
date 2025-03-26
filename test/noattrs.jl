65825405084447ffd2c1bd5301c8202dcc644dba
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-03-26 09:47:07 -0300

    Fix noattrrs.jl tests

--- test/noattrs.jl
+++ test/noattrs.jl
@@ -1,8 +1,7 @@
 @testset "GeoTables without attributes" begin
+  # Shapefile
   pset = [Point(0.0, 0.0), Point(1.0, 0.0), Point(0.0, 1.0)]
   gtb1 = georef(nothing, pset)
-
-  # Shapefile
   file = joinpath(savedir, "noattribs.shp")
   GeoIO.save(file, gtb1, warn=false)
   gtb2 = GeoIO.load(file)
@@ -10,6 +9,8 @@
   @test gtb2.geometry == gtb1.geometry
 
   # GeoParquet
+  pset = [Point(0.0, 0.0), Point(1.0, 0.0), Point(0.0, 1.0)]
+  gtb1 = georef(nothing, pset)
   file = joinpath(savedir, "noattribs.parquet")
   GeoIO.save(file, gtb1)
   gtb2 = GeoIO.load(file)
@@ -17,6 +18,8 @@
   @test gtb2 == gtb1
 
   # GeoPackage
+  pset = [Point(LatLon(30, 60)), Point(LatLon(30, 61)), Point(LatLon(31, 60))]
+  gtb1 = georef(nothing, pset)
   file = joinpath(savedir, "noattribs.gpkg")
   GeoIO.save(file, gtb1)
   gtb2 = GeoIO.load(file)
@@ -24,13 +27,17 @@
   @test gtb2 == gtb1
 
   # CSV
+  pset = [Point(0.0, 0.0), Point(1.0, 0.0), Point(0.0, 1.0)]
+  gtb1 = georef(nothing, pset)
   file = joinpath(savedir, "noattribs.csv")
   GeoIO.save(file, gtb1)
-  gtb2 = GeoIO.load(file, coords=[:x, :y])
+  gtb2 = GeoIO.load(file, coords=["x", "y"])
   @test isnothing(values(gtb2))
   @test gtb2 == gtb1
 
   # GSLIB
+  pset = [Point(0.0, 0.0), Point(1.0, 0.0), Point(0.0, 1.0)]
+  gtb1 = georef(nothing, pset)
   file = joinpath(savedir, "noattribs.gslib")
   GeoIO.save(file, gtb1)
   gtb2 = GeoIO.load(file)
@@ -38,18 +45,18 @@
   @test gtb2 == gtb1
 
   # VTK
+  grid = CartesianGrid(10, 10)
+  gtb1 = georef(nothing, grid)
   file = joinpath(savedir, "noattribs.vts")
-  gtb1 = georef(nothing, CartesianGrid(10, 10))
   GeoIO.save(file, gtb1)
   gtb2 = GeoIO.load(file)
   @test isnothing(values(gtb2))
   @test gtb2 == gtb1
 
   # MSH
-  gtb = GeoIO.load(joinpath(datadir, "tetrahedron1.msh"))
-  mesh = gtb.geometry
-  file = joinpath(savedir, "noattribs.msh")
+  mesh = GeoIO.load(joinpath(datadir, "tetrahedron1.msh")).geometry
   gtb1 = georef(nothing, mesh)
+  file = joinpath(savedir, "noattribs.msh")
   GeoIO.save(file, gtb1)
   gtb2 = GeoIO.load(file)
   @test isnothing(values(gtb2))
