07604f8f25a09ec836f1f3255bc69c42336627e1
Omar Elrefaei <elrefaei.omar@gmail.com>
2025-05-07 18:53:57 -0400

    testset fix

--- test/projjson.jl
+++ test/projjson.jl
@@ -1,14 +1,14 @@
 @testset "projjson" begin
   epsgcodes = Int[
     # https://github.com/JuliaEarth/CoordRefSystems.jl/blob/10b3f944ece7d5c4669eed6dc163ae8d9761afcd/src/get.jl
-    2157, 2193, 3035, 3310, 3395, 3857, 4171, 4207, 4208, 4230, 4231, 4267, 4269, 4274, 4275, 4277, 
+    2157, 2193, 3035, 3310, 3395, 3857, 4171, 4207, 4208, 4230, 4231, 4267, 4269, 4274, 4275, 4277,
     4314, 4326, 4618, 4659, 4666, 4668, 4674, 4745, 4746, 4988, 4989, 5070, 5324, 5527, 8086, 8232,
-    8237, 8240, 8246, 8249, 8252, 8255, 9777, 9782, 9988, 10176, 10414, 25832, 27700, 28355, 29903,       
+    8237, 8240, 8246, 8249, 8252, 8255, 9777, 9782, 9988, 10176, 10414, 25832, 27700, 28355, 29903,
     # 32662,  # deprecated in 2008, https://github.com/JuliaEarth/CoordRefSystems.jl/issues/262
     2180, 32600, 32700,
     
     # WKT strings with non-standard measurment units
-    27573, 3407,
+    3407,
     # WKT projjson with base_crs.datum.prime_meridian
     31288,
   ]
