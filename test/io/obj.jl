225841ce951bcda1ba85d25c1b4a9ebbb373731e
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-05-07 18:15:28 -0300

    Undo test/io/obj.jl change

--- test/io/obj.jl
+++ test/io/obj.jl
@@ -3,7 +3,7 @@
     gtb = GeoIO.load(joinpath(datadir, "tetrahedron.obj"))
     @test gtb.geometry isa SimpleMesh
     @test embeddim(gtb.geometry) == 3
-    @test Meshes.lentype(gtb.geometry) <: Meshes.Met
+    @test Meshes.lentype(gtb.geometry) <: Meshes.Met{Float64}
     @test eltype(gtb.geometry) <: Triangle
     @test length(gtb.geometry) == 4
 
