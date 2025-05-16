4c99cd0386678f799871e5352708b19bf25bb2dd
Omar Elrefaei <elrefaei.omar@gmail.com>
2025-05-16 12:34:36 -0400

    one round of cleanups for test/jsonutils.jl

--- test/jsonutils.jl
+++ test/jsonutils.jl
@@ -18,41 +18,29 @@ end
 # I can not figure out why tests fail without this!
 json_round_trip(j) = JSON3.read(JSON3.write(j), Dict)
 
-# Use DeepDiffs package to compare the different items between GDAL's projjson (j1) and our's (j2).
-# Diffrences are shown red/green color. Few insignificant keys are filtered from (j1),
-# though there are still more false-negatives than delta_paths filtering
-function projjson_deepdiff(j1::J, j2::J) where {J<:Union{Dict}}
-  nonsig_keys = ["bbox", "area", "scope", "usages", "\$schema"]
-  [delete!(j1, k) for k in nonsig_keys]
-  try
-    delete!(j1["base_crs"], "coordinate_system")
-  catch
-    nothing
-  end
-  diff = deepdiff(j1, j2)
-  return diff
-end
-
-# Return the json "paths" to the different objects between the two json inputs
-# By default, we clean the results from some optional projjson entries or minor
-# discrepancies with GDAL output. Set ignore_insig = true to disable that behavior.
-function delta_paths(j1, j2; ignore_insig::Bool=true)::Vector
+# Return the "paths" of objects that exhibit differences between the two json inputs.
+# By default, we clean the results from some optional projjson objects and minor
+# discrepancies with GDAL's output. Set ignore_insig = true to disable that behavior.
+# Note: find_diff_paths uses isapprox to compare numbers to avoid false negatives
+function delta_paths(j1, j2; ignore_insig=true)::Vector{String}
   diff_paths = find_diff_paths(j1, j2)
   ignore_insig == false && return diff_paths
 
   insig_keys = ["bbox", "area", "scope", "usages", "\$schema"]
-  # Occasionally GDAL's json doesn't have inverse_flattening and instead has semi_minor_axis
+
+  # Occasionally GDAL's ellipsoid doesn't have inverse_flattening and instead has semi_minor_axis
   #that is calculated from semi_major_axis * (1 - 1/inverse_flattening). Both cases are valid.
   push!(insig_keys, "datum.ellipsoid.semi_minor_axis")
   push!(insig_keys, "datum.ellipsoid.inverse_flattening")
-  # We don't get the optional node projcrs.base_crs.coordinate_system in our WKT, in contrast with GDAL
+
+  # We don't have the optional node base_crs.coordinate_system in our WKT, in contrast with GDAL
   push!(insig_keys, "base_crs.coordinate_system")
 
-  # delete insignificant json entries that are problematic for comparison
+  # delete insignificant json objects that are problematic for comparison testing
   for k in insig_keys
-    ind = findfirst(endswith(k), diff_paths)
-    if !isnothing(ind)
-      deleteat!(diff_paths, ind)
+    index = findfirst(endswith(k), diff_paths)
+    if !isnothing(index)
+      deleteat!(diff_paths, index)
     end
   end
 
@@ -66,7 +54,7 @@ function find_diff_paths(d1::Dict, d2::Dict, path="")
   paths = String[]
   all_keys = union(keys(d1), keys(d2))
   for key in all_keys
-    new_path = isempty(path) ? string(key) : string(path, ".", key)
+    new_path = string(path, ".", key)
     if haskey(d1, key) && haskey(d2, key)
       if !_isapprox(d1[key], d2[key])
         append!(paths, find_diff_paths(d1[key], d2[key], new_path))
@@ -89,7 +77,7 @@ function find_diff_paths(v1::Vector, v2::Vector, path)
       append!(paths, find_diff_paths(v1[i], v2[i], "$(path)[$(i)]"))
     end
   end
-
+  # extra elements, doesn't matter which vector
   for i in (min_length + 1):max_length
     push!(paths, "$(path)[$(i)]")
   end
@@ -97,7 +85,25 @@ function find_diff_paths(v1::Vector, v2::Vector, path)
 end
 
 function find_diff_paths(v1, v2, path)
-  _isapprox(v1, v2) ? String[] : [path]
+  _isapprox(v1, v2) ? nothing : [path]
+end
+
+# Use DeepDiffs package to view any differences between GDAL's projjson (j1) and our's (j2).
+# Differences are shown red/green color. Few insignificant keys are filtered from (j1),
+# though there are still more false-negatives than delta_paths filtering
+function projjson_deepdiff(j1::J, j2::J) where {J<:Union{Dict}}
+  j1 = deepcopy(j1)
+  nonsig_keys = ["bbox", "area", "scope", "usages", "\$schema"]
+  for k in nonsig_keys
+    delete!(j1, k)
+  end
+  try
+    delete!(j1["base_crs"], "coordinate_system")
+  catch
+    nothing
+  end
+  diff = deepdiff(j1, j2)
+  return diff
 end
 
 # For live development.
