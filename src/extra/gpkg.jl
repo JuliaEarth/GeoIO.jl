86a7c002b64410dedf432262426bd0dc56323eb5
Rishi Mondal <mavrickrishi@gmail.com>
2025-07-21 01:36:20 +0530

    Update src/extra/gpkg.jl
    Co-authored-by: Júlio Hoffimann <julio.hoffimann@gmail.com>

--- src/extra/gpkg.jl
+++ src/extra/gpkg.jl
@@ -2,8 +2,5 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-# Include GPKG reading functionality
 include("gpkg/read.jl")
-
-# Include GPKG writing functionality  
 include("gpkg/write.jl")
