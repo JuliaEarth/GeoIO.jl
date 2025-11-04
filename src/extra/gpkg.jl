72335cdea5582380017616ee3382007c36c2f01a
jph6366 <jphardee@gmail.com>
2025-11-04 16:29:39 -0500

    removed consts used for writer

--- src/extra/gpkg.jl
+++ src/extra/gpkg.jl
@@ -2,18 +2,5 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-# According to https://www.geopackage.org/spec/#r2
-# a GeoPackage should contain "GPKG" in ASCII in 
-# "application_id" field of SQLite db header
-const GPKG_APPLICATION_ID = Int(0x47504B47)
-const GPKG_1_4_VERSION = 10400
-
-# If the geometry type_name value is "GEOMETRY" 
-# then the feature table geometry column MAY contain:
-# geometries of any allowed geometry type. 
-function SQLite.sqlitetype_(::Type{Vector{UInt8}})
-  return "GEOMETRY"
-end
-
 include("gpkg/wkb.jl")
 include("gpkg/read.jl")
