76193b6be31bedf3f2bb4d9ce38c10d8953ad914
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-11-09 16:53:28 -0300

    More adjustments

--- src/extra/gpkg/wkb.jl
+++ src/extra/gpkg/wkb.jl
@@ -2,10 +2,10 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-function wkb2geom(io, crs)
+function wkb2geom(buff, crs)
   # Note: the coordinates are subject to byte order rules specified here
-  wkbbyteswap = isone(read(io, UInt8)) ? ltoh : ntoh
-  wkbtypebits = read(io, UInt32)
+  wkbbyteswap = isone(read(buff, UInt8)) ? ltoh : ntoh
+  wkbtypebits = read(buff, UInt32)
   # reader supports wkbGeometryType using the SFSQL 1.2 use offset of 1000 (Z) and SFSQL 1.1 that used a high-bit flag 0x80000000 (Z)
   if _haszextent(wkbtypebits)
     wkbtype = iszero(wkbtypebits & 0x80000000) ? wkbtypebits - 1000 : wkbtypebits & 0x7FFFFFFF
@@ -17,37 +17,37 @@ function wkb2geom(io, crs)
 
   if wkbtype > 3
     # 4 - 7 [MultiPoint, MultiLinestring, MultiPolygon, GeometryCollection]
-    wkb2multi(io, crs, zextent, wkbbyteswap)
+    wkb2multi(buff, crs, zextent, wkbbyteswap)
   else
     # 0 - 3 [Geometry, Point, Linestring, Polygon]
-    wkb2single(io, crs, wkbtype, zextent, wkbbyteswap)
+    wkb2single(buff, crs, wkbtype, zextent, wkbbyteswap)
   end
 end
 
 # read single features from Well-Known Binary IO Buffer and return Concrete Geometry
-function wkb2single(io, crs, wkbtype, zextent, bswap)
+function wkb2single(buff, crs, wkbtype, zextent, bswap)
   if wkbtype == 1
-    wkb2point(io, crs, zextent, bswap)
+    wkb2point(buff, crs, zextent, bswap)
   elseif wkbtype == 2
-    wkb2chain(io, crs, zextent, bswap)
+    wkb2chain(buff, crs, zextent, bswap)
   elseif wkbtype == 3
-    wkb2poly(io, crs, zextent, bswap)
+    wkb2poly(buff, crs, zextent, bswap)
   end
 end
 
-function wkb2point(io, crs, zextent, bswap)
-  y, x = bswap(read(io, Float64)), bswap(read(io, Float64))
+function wkb2point(buff, crs, zextent, bswap)
+  y, x = bswap(read(buff, Float64)), bswap(read(buff, Float64))
   if zextent
-    z = bswap(read(io, Float64))
+    z = bswap(read(buff, Float64))
     return x, y, z
   end
   Point(crs(x, y))
 end
 
-function wkb2chain(io, crs, zextent, bswap)
-  npoints = bswap(read(io, UInt32))
+function wkb2chain(buff, crs, zextent, bswap)
+  npoints = bswap(read(buff, UInt32))
   chain = map(1:npoints) do _
-    wkb2point(io, crs, zextent, bswap)
+    wkb2point(buff, crs, zextent, bswap)
   end
   if length(chain) >= 2 && first(chain) != last(chain)
     Rope(chain)
@@ -56,25 +56,25 @@ function wkb2chain(io, crs, zextent, bswap)
   end
 end
 
-function wkb2poly(io, crs, zextent, bswap)
-  nrings = bswap(read(io, UInt32))
+function wkb2poly(buff, crs, zextent, bswap)
+  nrings = bswap(read(buff, UInt32))
   rings = map(1:nrings) do _
-    wkb2chain(io, crs, zextent, bswap)
+    wkb2chain(buff, crs, zextent, bswap)
   end
   PolyArea(rings)
 end
 
-function wkb2multi(io, crs, zextent, bswap)
-  ngeoms = bswap(read(io, UInt32))
+function wkb2multi(buff, crs, zextent, bswap)
+  ngeoms = bswap(read(buff, UInt32))
   geoms = map(1:ngeoms) do _
-    wkbbswap = isone(read(io, UInt8)) ? ltoh : ntoh
-    wkbtypebits = read(io, UInt32)
+    wkbbswap = isone(read(buff, UInt8)) ? ltoh : ntoh
+    wkbtypebits = read(buff, UInt32)
     # if 2D+Z the dimensionality flag is present
     if _haszextent(wkbtypebits)
       wkbtype = iszero(wkbtypebits & 0x80000000) ? wkbtypebits - 1000 : wkbtypebits & 0x7FFFFFFF
-      wkb2single(io, crs, wkbtype, true, wkbbswap)
+      wkb2single(buff, crs, wkbtype, true, wkbbswap)
     else
-      wkb2single(io, crs, wkbtypebits, false, wkbbswap)
+      wkb2single(buff, crs, wkbtypebits, false, wkbbswap)
     end
   end
   Multi(geoms)
